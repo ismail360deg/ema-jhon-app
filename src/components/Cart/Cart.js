8b06371ef4586331d77dd4e3de0f750df351b993
ismail360deg <ismailbhuiyan2000@gmail.com>
2022-09-26 11:36:56 +0600

    first commit

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cart = ({ cart }) => {
+    return (
+        <div>
+            <h4>Order Summary</h4>
+            <p>Selected Items: {cart.length}</p>
+        </div>
+    );
+};
+export default Cart;
